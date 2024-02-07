2b1982c8fab6298cd5b2c804f33c48cceceaf3d4
jennaly <106183040+jennaly@users.noreply.github.com>
2024-02-07 12:01:25 -0800

    Set up context providers for app

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { FavoriteWordsContextProvider } from "./context/FavoriteWordsContext";
+import { WordDataContextProvider } from "./context/WordDataContext";
+import { AuthContextProvider } from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <FavoriteWordsContextProvider>
+        <WordDataContextProvider>
+          <App />
+        </WordDataContextProvider>
+      </FavoriteWordsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
