4445d3eaf7c4c8c129d681311aee411a986f9136
jennaly <106183040+jennaly@users.noreply.github.com>
2024-02-07 16:01:25 -0800

    create downloadButton component

---
+++ src/components/DownloadButton.tsx
@@ -0,0 +1,153 @@
+import { useState, useEffect } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useFavoriteWordsContext } from "../hooks/useFavoriteWordsContext";
+
+const DownloadButtons = () => {
+  const { user } = useAuthContext();
+  const { favoriteWords } = useFavoriteWordsContext();
+
+  const [name, setName] = useState("");
+  const [showNameInput, setShowNameInput] = useState(false);
+  const todayDate = new Date().toISOString().slice(0, 10);
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+    }
+  }, [user]);
+
+  const handleClick = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    cb: (e: React.MouseEvent<HTMLButtonElement>, formattedName: string) => void
+  ) => {
+    if (!name) {
+      setShowNameInput(true);
+    } else {
+      const formattedName = formatName(name);
+      cb(e, formattedName);
+      setShowNameInput(false);
+    }
+  };
+
+  const downloadFile = ({
+    data,
+    fileName,
+    fileType,
+  }: {
+    data: string;
+    fileName: string;
+    fileType: string;
+  }) => {
+    const blob = new Blob([data], { type: fileType });
+
+    const a = document.createElement("a");
+    a.download = fileName;
+    a.href = window.URL.createObjectURL(blob);
+    const clickEvt = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
+    a.dispatchEvent(clickEvt);
+    a.remove();
+  };
+
+  const formatName = (fullName: string) => {
+    return fullName
+      .split(" ")
+      .map((word: string, index: number) => (index == 0 ? word : word[0]))
+      .join("");
+  };
+
+  const exportToStudySetCsv = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    formattedName: string
+  ) => {
+    e.preventDefault();
+
+    // Headers for each column
+    let headers = ["Term", "Definitions"];
+
+    // Convert users data to a csv
+    let vocabWordsCsv = favoriteWords.reduce((acc: string[][], word) => {
+      const { term, definitions } = word;
+
+      // Escape commas in definition strings
+      const newDefinitions = definitions.map((definition) => `"${definition}"`);
+
+      acc.push([term, newDefinitions.join(",")]);
+      return acc;
+    }, []);
+
+    downloadFile({
+      data: [headers, ...vocabWordsCsv].join("\n"),
+      fileName: `${todayDate}-StudySet-${formattedName}.csv`,
+      fileType: "text/csv",
+    });
+  };
+
+  const exportToReportCsv = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    formattedName: string
+  ) => {
+    e.preventDefault();
+
+    // Headers for each column
+    let headers = ["Term", "Difficulty"];
+
+    // Convert users data to a csv
+    let vocabWordsCsv = favoriteWords.reduce((acc: string[][], word) => {
+      const { term, difficulty } = word;
+
+      acc.push([term, difficulty ? String(difficulty) : ""]);
+      return acc;
+    }, []);
+
+    downloadFile({
+      data: [headers, ...vocabWordsCsv].join("\n"),
+      fileName: `${todayDate}-Report-${formattedName}.csv`,
+      fileType: "text/csv",
+    });
+  };
+
+  return (
+    <>
+      {showNameInput && (
+        <div className="alert alert-warning shadow-lg">
+          <div className="flex font-gaegu text-lg lg:text-xl">
+            <label htmlFor="name" className="font-gaegu text-yellow-700 ">
+              Please enter your name:
+              <input
+                id="name"
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Miles Morales"
+                value={name}
+                className="bg-transparent p-0.5 ml-1"
+              />
+            </label>
+          </div>
+        </div>
+      )}
+      <div className="flex max-w-sm lg:max-w-xl mx-auto justify-center gap-5 m-5 text-sm lg:text-base">
+        <button
+          type="button"
+          onClick={(e) => handleClick(e, exportToStudySetCsv)}
+          className="rounded-none lg:rounded-full bg-yellow-700 hover:bg-yellow-800 hover:border-yellow-800 font-fredoka-one text-base-100 uppercase"
+        >
+          Download Study Set
+        </button>
+
+        <button
+          type="button"
+          onClick={(e) => handleClick(e, exportToReportCsv)}
+          className="rounded-none lg:rounded-full border-2 border-yellow-700 hover:border-yellow-800  text-yellow-700 hover:text-yellow-800 font-fredoka-one uppercase"
+        >
+          Download Teacher's Report
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default DownloadButtons;
