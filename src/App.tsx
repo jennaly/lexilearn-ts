82887373961cd900275f24a9728f972bddaf2012
jennaly <106183040+jennaly@users.noreply.github.com>
2024-02-09 17:06:35 -0800

    create main application file and set up routing between homepage, login, and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+import Layout from "./components/Layout";
+
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
-    <>
-      <div>
-        <p className="text-blue-700">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-    </>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
+
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
